841fa8a3caabefc9e3a72dd0e9ed50375ed1d5de
Björn Hellander <bjorn.hellander@gmail.com>
2022-05-08 14:07:16 +0200

    Initial commit (Test passes)

---
+++ RefactoringTestProblem1.Refactoring/MyCodeRefactoringProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using System.Composition;
+using System.Threading.Tasks;
+
+namespace RefactoringTestProblem1
+{
+    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = nameof(MyCodeRefactoringProvider)), Shared]
+    public class MyCodeRefactoringProvider : CodeRefactoringProvider
+    {
+        public sealed override Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ RefactoringTestProblem1.Test/UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using static RefactoringTestProblem1.Test.CSharpCodeRefactoringVerifier<
+    RefactoringTestProblem1.MyCodeRefactoringProvider>;
+
+namespace RefactoringTestProblem1.Test
+{
+    [TestClass]
+    public class MyUnitTest
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"[||]";
+
+            await VerifyRefactoringAsync(test, test);
+        }
+    }
+}
